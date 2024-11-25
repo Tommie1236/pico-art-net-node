8d934f6da3709bdbc6ba1248b9f37dbb519c1359
Timo Oosterom (@tommie1236) <90513578+Tommie1236@users.noreply.github.com>
2024-11-25 22:29:23 +0100

    moved the test from main to a test file

---
+++ Code/src/firmware/dmx-test.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
+#include "pico/time.h"
+
+#include "hardware/adc.h"
+
+#include "main.h"
+
+#include <DmxOutput.h>
+
+DmxOutput dmx;
+
+uint8_t universe[UNIVERSE_LENGTH + 1];
+
+
+int main () {
+
+    adc_init();
+    adc_gpio_init(26);
+    adc_gpio_init(27);
+
+
+    dmx.begin(0);
+    universe[1] = 0;        // red
+    universe[2] = 255;      // green
+    universe[3] = 0;        // blue
+    universe[4] = 0;        // white
+    universe[5] = 100;      // dimmer
+
+
+while (1) {
+    dmx.write(universe, UNIVERSE_LENGTH);
+    while (dmx.busy()) {}
+
+    adc_select_input(0);
+    universe[1] = adc_read() / 16;
+    adc_select_input(1);
+    universe[3] = adc_read() / 16;
+
+}
+};
