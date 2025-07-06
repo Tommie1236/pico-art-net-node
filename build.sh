68c32f1deef2137eec80bf94af9a432c1255b612
timoo <90513578+Tommie1236@users.noreply.github.com>
2025-07-07 00:16:17 +0200

    changed picotool load to be more efficient by not overwriting matching portions of flash

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ make -j $(($(nproc) * 2))
 while getopts 'l' flag; do
   case "${flag}" in (l) 
       echo "Loading firmware <$(pwd)/src/firmware/firmware.bin> to RP2040"
-      sudo picotool load $(pwd)/src/firmware/firmware.bin -f
+      sudo picotool load $(pwd)/src/firmware/firmware.bin -f --update
       ;;
   esac
 done
