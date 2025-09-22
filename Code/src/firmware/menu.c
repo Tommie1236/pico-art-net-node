0fb3de9934bdfb896959c76aa0778d8d96e0a0f2
timoo <90513578+Tommie1236@users.noreply.github.com>
2025-09-22 22:49:21 +0200

    progress in menu state machine

--- Code/src/firmware/menu.c
+++ Code/src/firmware/menu.c
@@ -3,7 +3,7 @@
 #include "menu.h"
 
 
-void menu_enter_menu(menu_t *menu) {
+goid menu_enter_menu(const menu_t *menu) {
     menu_stack_push(menu);
     menu_state.edit = 0;
 }
@@ -15,24 +15,103 @@ void menu_back() {
 
 void menu_reset() {
     menu_state.stack_pointer = 0;
-    // TODO: change NULL to the root menu!
+
+    // TODO: change NULL to the root menu! ------ v
     menu_state.stack[menu_state.stack_pointer] = NULL;
     menu_state.selected_index[menu_state.stack_pointer] = 0;
     menu_state.edit = 0;
 }
 
-void menu_stack_push(menu_t *menu) {
+void menu_stack_push(const menu_t *menu) {
     if (menu_state.stack_pointer < MENU_STACK_SIZE - 1) {
         menu_state.stack[++menu_state.stack_pointer] = menu;
     }
 }
 
-void menu_stack_pop() {
+const menu_t* menu_stack_pop() {
     if (menu_state.stack_pointer > 0) {
-        menu_state.stack_pointer--;
+
+        // reset selected index shen moving back up
+        menu_state.selected_index[menu_state.stack_pointer] = 0;
+
+        return menu_state.stack[menu_state.stack_pointer--];
+    }
+    return menu_state.stack[menu_state.stack_pointer];
+}
+
+const menu_t* menu_stack_current() {
+
+    return menu_state.stack[menu_state.stack_pointer];
+};
+
+void menu_button_menu() {
+
+    const menu_t *menu = menu_stack_current();
+    const menu_item_t *selected_item = menu->items + menu_state.selected_index[menu_state.stack_pointer];
+    const menu_item_type_t item_type = selected_item->item_type;
+
+    switch(item_type) {
+        case MENU_I_SUBMENU: 
+            menu_enter_menu(selected_item->submenu);
+            break;
+
+        case MENU_I_TOGGLE:
+            // selected_item->value = !selected_item->value;
+            break;
+
+        case MENU_I_RADIO:
+            // uint8_t xxx = menu_state.selected_index[menu_state.stack_pointer];
+            break;
+
+        case MENU_I_INT:
+            if (menu_state.edit == 0) {
+                menu_state.edit = 1;
+            } else {
+                menu_state.edit = 0;
+            }
+            break;
+
+        case MENU_I_MULTI_INT:
+            break;
+
+        case MENU_I_STRING:
+            break;
     }
 }
 
-menu_t* menu_stack_current() {
-    return menu_state.stack[menu_state.stack_pointer];
-};
+
+void menu_button_exit() {
+    if (menu_state.edit) {
+        
+        // TODO: fine with single value edits but needs other option for
+        // multi int and or string edit.
+        menu_state.edit = 0;
+    } else {
+        menu_back();
+        return;        
+    }
+}
+
+void menu_button_up() {
+    if (menu_state.edit) {
+        // change value
+    } else {
+        if (menu_state.selected_index[menu_state.stack_pointer] <= 1) {
+            menu_state.selected_index[menu_state.stack_pointer] = menu_state.stack[menu_state.stack_pointer]->num_items - 1;
+        } else menu_state.selected_index[menu_state.stack_pointer]--;
+    }
+}
+
+void menu_button_down() {
+    if (menu_state.edit) {
+        // change value
+    } else {
+        if (menu_state.selected_index[menu_state.stack_pointer] >= 2) {
+            menu_state.selected_index[menu_state.stack_pointer] = 0;
+        } else menu_state.selected_index[menu_state.stack_pointer]++;
+    }
+}
+
+void menu_draw_menu() {
+
+}
--- Code/src/firmware/menu.h
+++ Code/src/firmware/menu.h
@@ -10,33 +10,44 @@
 
 // Menu item types:
 typedef enum {
-    MENU_SUBMENU,
-    MENU_TOGGLE,
-    MENU_RADIO,
-    MENU_INT,
-    MENU_MULTI_INT,
-    MENU_STRING,
+    MENU_I_SUBMENU,     // goto submenu
+    MENU_I_TOGGLE,      // checkbox / toggle value
+    MENU_I_RADIO,       // item of multiple choice
+    MENU_I_INT,
+    MENU_I_MULTI_INT,
+    MENU_I_STRING,
 } menu_item_type_t;
 
 struct menu_t;
 
-// Menu item:
-typedef struct {
-    const char* label;
+// Menu item, the individual items in a visible menu:
+typedef struct menu_item_t {
+
+    // Could fit 10 but menu items are indented and or
+    // have a icon in front of it.
+    const char* label[9];
     const menu_item_type_t item_type;
 
-    const struct menu_t* submenu; // Type: Submenu.
-    // Radio menu special type?
-    const void *value; 
+    // Only for type Submenu. else leave empty
+    const struct menu_t* submenu;
+    // TODO: support radio menu
+
+    void* value;
+
+    const void* callback;
+    const void* callback_args;
+
+    // Show menu item striketought and don't allow interaction
+    // when not active. looke like not implemented yet
     bool active;
     
 } menu_item_t;
 
-// Menu:
-typedef struct {
-    const char* title;
-    const menu_item_t* items;
-    const uint8_t num_items;
+// Menu, what you see and has items.
+typedef struct menu_t {
+    const char* title[10];      // 12x16 font. 128 // 12 = 10
+    const menu_item_t* items;   // Pointer to array of menu items.
+    const uint8_t num_items;    // number of items in said array.
 } menu_t;
 
 
@@ -46,35 +57,59 @@ typedef struct {
 #define MENU_STACK_SIZE 10
 
 typedef struct {
-    menu_t* stack[MENU_STACK_SIZE];
+    // menu nesting stack
+    const menu_t* stack[MENU_STACK_SIZE];
     uint8_t stack_pointer;
+
+    // save the index of the selected item for each of the menu's
+    // visited
     uint8_t selected_index[MENU_STACK_SIZE];
-    uint8_t edit;   // 0 = browse, 1(+) edit field index
+
+    // 0 = browse
+    // 1 = edit current item 
+    // 1(+) = edit item + index. eg part of ip addr.
+    uint8_t edit;
 } menu_state_t;
 
 extern menu_state_t menu_state;
 
-void menu_task();
 
-void menu_enter_menu(menu_t *menu);
+// enter submenu / enter arbirary menu and add to menu stack
+void menu_enter_menu(const menu_t *menu);
 
+// go one menu back
 void menu_back();
 
+// go back to root menu
 void menu_reset();
 
-void menu_stack_push(menu_t *menu);
+// push menu onto stack / set current menu
+void menu_stack_push(const menu_t *menu);
 
-void menu_stack_pop();
+// pop menu from stack / go one menu back
+const menu_t* menu_stack_pop();
 
-menu_t* menu_stack_current();
+// get current menu from stack
+const menu_t* menu_stack_current();
 
+// trigger action linked to [menu] button
 void menu_button_menu();
 
+// trigger action linked to [up] button
 void menu_button_up();
 
+// trigger action linked to [down] button
 void menu_button_down();
 
+// trigger action linked to [exit] button
 void menu_button_exit();
 
 
+
+// menu state machine.
+void menu_draw_menu();
+
+void menu_process_menu();
+
 #endif // MENU_H
+
